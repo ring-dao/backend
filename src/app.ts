ac089005d4239e32e106ca9f958f3532e3fb8050
Maxime <sowlution.pro@gmail.com>
2024-04-29 20:01:53 -0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express, { Express } from 'express';
+import cors from 'cors'; // Import the cors module
 import { connectToDatabase } from './utils';
 import router from './router';
 require('dotenv').config();
 
 const app: Express = express();
+
+// Enable CORS for all requests
+app.use(cors());
+
+// Or, to enable CORS for specific origins, you can do:
+// app.use(cors({
+//   origin: 'https://www.yourdomain.com'
+// }));
+
 app.use(express.json());
 console.log(process.env.DB_URL);
 connectToDatabase(process.env.DB_URL ?? "");
 app.use(router);
+
 export default app;
